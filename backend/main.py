4ea23dcf4ca145365cf192f6783c0b21843ff8e2
wannalina <annalina_wheeler@hotmail.com>
2025-11-14 22:38:55 +0200

    create fastapi server

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+# run FastAPI backend using command: fastapi dev main.py
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
